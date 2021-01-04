2fc2d17773433f30f962432ad7512fc2ca0e5f3d
britto1996 <61541167+britto1996@users.noreply.github.com>
2021-01-04 14:41:28 +0530

    updated user file

--- controllers/user.js
+++ controllers/user.js
@@ -1,1 +1,21 @@
 const User = require("../models/user")
+
+
+exports.getUserId = (req,res,next,_id)=>{
+    User.findById(_id)
+    .exec((err,user)=>{
+        if(err || !user){
+            res.json({
+                message:"not a valid user"
+            })
+        }
+        if(user){
+            req.profile = user
+        }
+    })
+    next()
+}
+
+exports.getUser = (req,res)=>{
+    res.json(req.profile)
+}
